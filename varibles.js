b6e5f76cfbe5138cb507d9e53d29fba75ca0d24a
kolotskiI <ishielmishm24@gmail.com>
2024-09-13 22:08:29 +0800

    Initial commit

---
+++ varibles.js
@@ -0,0 +1,8 @@
+let name ="Ishi";
+let whatDoYoWannaBecomeInYourLife = "A programmer";
+let gender = "Female";
+let twitterHandle = "none";
+console.log(name);
+console.log(whatDoYoWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
